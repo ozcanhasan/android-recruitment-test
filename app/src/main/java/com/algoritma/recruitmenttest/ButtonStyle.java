97bb087c4070e34d3f67ff3b423381e07b0a8032
Hasan Ozcan <ozcanhasan@yahoo.com>
2024-02-08 01:47:06 +0300

    kenarlık-222222

--- app/src/main/java/com/algoritma/recruitmenttest/ButtonStyle.java
+++ app/src/main/java/com/algoritma/recruitmenttest/ButtonStyle.java
@@ -2,7 +2,7 @@ package com.algoritma.recruitmenttest;
 
 public class ButtonStyle {
     //renk değiştir me **************
-    //kenarlık koy
+    //kenarlık koy - 222222222
     //arkaplan değiştir
     //font değiştir
 }
